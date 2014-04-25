b4c249b8e09ca0fcc9a043ad6fafba0a29a62a38
Tobias Kuhn <kuhntobias@gmail.com>
2014-04-25 09:52:21 +0200

    Make sure prefixes are split at '.' when writing TriG

--- src/main/java/org/nanopub/CustomTrigWriter.java
+++ src/main/java/org/nanopub/CustomTrigWriter.java
@@ -6,6 +6,7 @@ import java.io.Writer;
 
 import org.openrdf.model.URI;
 import org.openrdf.rio.trig.TriGWriter;
+import org.openrdf.rio.turtle.TurtleUtil;
 
 /**
  * @author Tobias Kuhn
@@ -20,13 +21,40 @@ public class CustomTrigWriter extends TriGWriter {
 		super(writer);
 	}
 
+	@Override
 	protected void writeURI(URI uri) throws IOException {
-		String prefix = namespaceTable.get(uri.toString());
+		String uriString = uri.toString();
+
+		String prefix = namespaceTable.get(uriString);
 		if (prefix != null) {
+			// Exact match: no suffix required
 			writer.write(prefix);
 			writer.write(":");
+			return;
+		}
+
+		prefix = null;
+
+		int splitIdxNorm = TurtleUtil.findURISplitIndex(uriString);
+		// Do also split at dots:
+		int splitIdxDot = uriString.lastIndexOf(".") + 1;
+		if (uriString.length() == splitIdxDot) splitIdxDot = -1;
+		int splitIdx = Math.max(splitIdxNorm, splitIdxDot);
+		if (splitIdx > 0) {
+			String namespace = uriString.substring(0, splitIdx);
+			prefix = namespaceTable.get(namespace);
+		}
+
+		if (prefix != null) {
+			// Namespace is mapped to a prefix; write abbreviated URI
+			writer.write(prefix);
+			writer.write(":");
+			writer.write(uriString.substring(splitIdx));
 		} else {
-			super.writeURI(uri);
+			// Write full URI
+			writer.write("<");
+			writer.write(TurtleUtil.encodeURIString(uriString));
+			writer.write(">");
 		}
 	}
 
