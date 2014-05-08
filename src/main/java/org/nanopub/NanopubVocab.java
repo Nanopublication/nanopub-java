76a67b9740049819d0fc5dc424dd186ded6b062e
Tobias Kuhn <kuhntobias@gmail.com>
2014-05-08 12:21:12 +0200

    Fix PROV-O URI and add wasAttributedTo

--- src/main/java/org/nanopub/NanopubVocab.java
+++ src/main/java/org/nanopub/NanopubVocab.java
@@ -15,7 +15,7 @@ public class NanopubVocab {
 	public static final URI XSD_DATETIME = new URIImpl("http://www.w3.org/2001/XMLSchema#dateTime");
 
 	public static final URI DCT_CREATED = new URIImpl("http://purl.org/dc/terms/created");
-	public static final URI PROV_GENERATEDATTIME = new URIImpl("http://www.w3.org/TR/prov-o/#generatedAtTime");
+	public static final URI PROV_GENERATEDATTIME = new URIImpl("http://www.w3.org/ns/prov#generatedAtTime");
 
 	public static boolean isCreationTimeProperty(URI uri) {
 		return uri.equals(DCT_CREATED) || uri.equals(PROV_GENERATEDATTIME);
@@ -25,12 +25,14 @@ public class NanopubVocab {
 	public static final URI PAV_CREATEDBY_1 = new URIImpl("http://swan.mindinformatics.org/ontologies/1.2/pav/createdBy");
 	public static final URI DCT_CREATOR = new URIImpl("http://purl.org/dc/terms/creator");
 	public static final URI DCE_CREATOR = new URIImpl("http://purl.org/dc/elements/1.1/creator");
+	public static final URI PROV_WASATTRIBUTEDTO = new URIImpl("http://www.w3.org/ns/prov#wasAttributedTo");
 
 	public static final URI PAV_AUTHOREDBY = new URIImpl("http://purl.org/pav/authoredBy");
 	public static final URI PAV_AUTHOREDBY_1 = new URIImpl("http://swan.mindinformatics.org/ontologies/1.2/pav/authoredBy");
 
 	public static boolean isCreatorProperty(URI uri) {
-		return uri.equals(PAV_CREATEDBY) || uri.equals(PAV_CREATEDBY_1) || uri.equals(DCT_CREATOR) || uri.equals(DCE_CREATOR);
+		return uri.equals(PAV_CREATEDBY) || uri.equals(PAV_CREATEDBY_1) || uri.equals(DCT_CREATOR) || uri.equals(DCE_CREATOR)
+				|| uri.equals(PROV_WASATTRIBUTEDTO);
 	}
 
 	public static boolean isAuthorProperty(URI uri) {
