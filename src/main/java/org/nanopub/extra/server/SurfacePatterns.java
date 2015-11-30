d91ebd4dd65f982e2ca48afa3bda3655d49f1366
Tobias Kuhn <kuhntobias@gmail.com>
2015-11-30 16:55:21 +0100

    Add methods

--- src/main/java/org/nanopub/extra/server/SurfacePatterns.java
+++ src/main/java/org/nanopub/extra/server/SurfacePatterns.java
@@ -53,4 +53,51 @@ public class SurfacePatterns {
 		return match;
 	}
 
+	public boolean overlapsWith(SurfacePatterns other) {
+		// TODO This is inefficient: improve!
+		if (uriPattern != null && other.uriPattern != null) {
+			boolean overlapEncountered = false;
+			for (String p1 : uriPattern) {
+				for (String p2 : other.uriPattern) {
+					if (p1.startsWith(p2) || p2.startsWith(p1)) {
+						overlapEncountered = true;
+						break;
+					}
+				}
+				if (overlapEncountered) break;
+			}
+			if (!overlapEncountered) return false;
+		}
+		if (hashPattern != null && other.hashPattern != null) {
+			boolean overlapEncountered = false;
+			for (String p1 : hashPattern) {
+				for (String p2 : other.hashPattern) {
+					if (p1.startsWith(p2) || p2.startsWith(p1)) {
+						overlapEncountered = true;
+						break;
+					}
+				}
+				if (overlapEncountered) break;
+			}
+			if (!overlapEncountered) return false;
+		}
+		return true;
+	}
+
+	public static boolean matchesUri(String uri, String uriPattern, String hashPattern) {
+		return new SurfacePatterns(uriPattern, hashPattern).matchesUri(uri);
+	}
+
+	public static boolean matchesUri(String uri, ServerInfo serverInfo) {
+		return new SurfacePatterns(serverInfo).matchesUri(uri);
+	}
+
+	public static boolean matchesHash(String artifactCode, String uriPattern, String hashPattern) {
+		return new SurfacePatterns(uriPattern, hashPattern).matchesHash(artifactCode);
+	}
+
+	public static boolean matchesHash(String artifactCode, ServerInfo serverInfo) {
+		return new SurfacePatterns(serverInfo).matchesHash(artifactCode);
+	}
+
 }
