62eb23f3bb1c790b82cb252b410fab9763a64b1e
Tobias Kuhn <kuhntobias@gmail.com>
2019-01-02 15:07:10 +0100

    Add code to retrieve introduction nanopub from user ID

---
+++ src/main/java/org/nanopub/extra/security/GetIntroNanopub.java
@@ -0,0 +1,113 @@
+package org.nanopub.extra.security;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.nanopub.NanopubImpl;
+import org.openrdf.OpenRDFException;
+import org.openrdf.model.Statement;
+import org.openrdf.model.vocabulary.FOAF;
+import org.openrdf.rio.RDFHandlerException;
+import org.openrdf.rio.helpers.RDFHandlerBase;
+import org.openrdf.rio.turtle.TurtleParser;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
+
+import net.trustyuri.TrustyUriUtils;
+
+public class GetIntroNanopub {
+
+	@com.beust.jcommander.Parameter(description = "user-id", required = true)
+	private List<String> userIds;
+
+	public static void main(String[] args) {
+		NanopubImpl.ensureLoaded();
+		GetIntroNanopub obj = new GetIntroNanopub();
+		JCommander jc = new JCommander(obj);
+		try {
+			jc.parse(args);
+		} catch (ParameterException ex) {
+			jc.usage();
+			System.exit(1);
+		}
+		try {
+			obj.run();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	public static String get(String userId) throws IOException, OpenRDFException {
+		RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(10000)
+				.setConnectionRequestTimeout(100).setSocketTimeout(10000).build();
+		HttpClient c = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig).build();
+		return get(userId, c);
+	}
+
+	public static String get(final String userId, HttpClient httpClient) throws IOException, OpenRDFException {
+		HttpGet get = new HttpGet(userId);
+		get.setHeader("Accept", "text/turtle");
+		InputStream in = null;
+		try {
+			HttpResponse resp = httpClient.execute(get);
+			if (!wasSuccessful(resp)) {
+				EntityUtils.consumeQuietly(resp.getEntity());
+				throw new IOException(resp.getStatusLine().toString());
+			}
+			in = resp.getEntity().getContent();
+			IntroExtractor ie = new IntroExtractor(userId);
+			TurtleParser parser = new TurtleParser();
+			parser.setRDFHandler(ie);
+			parser.parse(in, userId);
+			return ie.getIntroNanopubUri();
+		} finally {
+			if (in != null) in.close();
+		}
+	}
+
+	private static class IntroExtractor extends RDFHandlerBase {
+
+		private String userId;
+		private String introNanopubUri;
+
+		public IntroExtractor(String userId) {
+			this.userId = userId;
+		}
+
+		public void handleStatement(Statement st) throws RDFHandlerException {
+			if (introNanopubUri != null) return;
+			if (!st.getSubject().stringValue().equals(userId)) return;
+			if (!st.getPredicate().stringValue().equals(FOAF.PAGE.stringValue())) return;
+			String o = st.getObject().stringValue();
+			if (TrustyUriUtils.isPotentialTrustyUri(o)) {
+				introNanopubUri = o;
+			}
+		};
+
+		public String getIntroNanopubUri() {
+			return introNanopubUri;
+		}
+	}
+	
+	private static boolean wasSuccessful(HttpResponse resp) {
+		int c = resp.getStatusLine().getStatusCode();
+		return c >= 200 && c < 300;
+	}
+
+	private void run() throws IOException, OpenRDFException {
+		for (String userId : userIds) {
+			System.out.print(userId + ": ");
+			System.out.println(get(userId));
+		}
+	}
+
+}
