3622afa260d7941ae709b36fe0a711bf0856d80c
Tobias Kuhn <kuhntobias@gmail.com>
2022-11-07 15:45:40 +0100

    Add comment to script

--- initialize.sh
+++ initialize.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+#
+# This script is needed for the Maven Release Plugin to load the
+# content of the Git submodules (testsuite) too.
+
 git submodule update --init
 git submodule foreach git submodule update --init
