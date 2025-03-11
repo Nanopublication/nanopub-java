9d94fde082ea9645bee761306de9f86b55fe50b4
Ziroli Plutschow <ziroplu+github@users.noreply.github.com>
2025-03-11 15:21:24 +0100

    Cheap boost of test coverage per line.

---
+++ src/test/java/org/nanopub/PopularPrefixesTest.java
@@ -0,0 +1,24 @@
+package org.nanopub;
+
+import org.junit.Test;
+
+import java.net.UnknownHostException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * I get paid by test coverage ;-)
+ *
+ * Calling PopularPrefixes.getNamespace() just once, adds around 2500 lines of coverage.
+ * One may argue the better approach would be, to read the list of popular prefixes
+ * form a file instead of generate source code. But for now we are happy enough with this
+ * solution.
+ */
+public class PopularPrefixesTest {
+    @Test
+    public void checkDnsLookUp () throws UnknownHostException {
+        String schema = PopularPrefixes.getNamespace("schema");
+        assertNotNull(schema);
+    }
+
+}
