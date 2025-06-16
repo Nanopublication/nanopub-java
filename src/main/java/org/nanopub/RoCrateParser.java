f64859ae663ae7970270c73e3bd2f59d289783c9
Ziroli Plutschow <ziroplu+github@users.noreply.github.com>
2025-06-16 15:25:48 +0200

    Add example Nanopubs from RO-Crate

--- src/main/java/org/nanopub/RoCrateParser.java
+++ src/main/java/org/nanopub/RoCrateParser.java
@@ -23,20 +23,28 @@ import java.net.http.HttpResponse;
 
 public class RoCrateParser {
 
-    public final static String RO_CRATE_METADATA = "ro-crate-metadata.json";
     private HttpClient client = HttpClient.newHttpClient();
     private final ValueFactory vf = SimpleValueFactory.getInstance();
     private boolean verbose = false;
 
-    public Nanopub parseRoCreate(String url) throws MalformedNanopubException, IOException, InterruptedException, URISyntaxException {
-        HttpRequest req = HttpRequest.newBuilder().GET().uri(new URI(url + RO_CRATE_METADATA)).build();
+    /**
+     * @param url the url where the metadata file is published (including trailing "/")
+     * @param metadataFile the name of the metadata file.
+     */
+    public Nanopub parseRoCreate(String url, String metadataFile) throws MalformedNanopubException, IOException, InterruptedException, URISyntaxException {
+        HttpRequest req = HttpRequest.newBuilder().GET().uri(new URI(url + metadataFile)).build();
         HttpResponse<InputStream> httpResponse = client.send(req, HttpResponse.BodyHandlers.ofInputStream());
 
 
         RDFParser parser = Rio.createParser(RDFFormat.JSONLD);
-        // Optional: Configure parser settings
+
+        // Configure parser settings
         parser.getParserConfig().set(BasicParserSettings.VERIFY_DATATYPE_VALUES, true);
-        // Since JSONLDSettings.WHITELIST does not contain "https://w3id.org/ro/crate/1.0/context"
+
+        // We do not accept spaces in urls (@id elements)
+        // parser.getParserConfig().set(BasicParserSettings.VERIFY_URI_SYNTAX, false);
+
+        // Since JSONLDSettings.WHITELIST does not contain "https://w3id.org/ro/crate/1.0/context" we disable SECURE MODE
         parser.getParserConfig().set(JSONLDSettings.SECURE_MODE, false);
 
         Model model = new LinkedHashModel();
@@ -54,7 +62,7 @@ public class RoCrateParser {
         NanopubCreator npCreator = new NanopubCreator(true);
         npCreator.addAssertionStatements(handler.getStatements());
 
-        npCreator.addProvenanceStatement(PROV.WAS_DERIVED_FROM, vf.createIRI(url+RO_CRATE_METADATA));
+        npCreator.addProvenanceStatement(PROV.WAS_DERIVED_FROM, vf.createIRI(url+ metadataFile));
         npCreator.addPubinfoStatement(RDF.TYPE, vf.createIRI("http://purl.org/nanopub/x/ExampleRoCreateNanopub"));
 
         return npCreator.finalizeNanopub(true);
