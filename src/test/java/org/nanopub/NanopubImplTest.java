a8f4854c7a39486cecf70d11add3320f1e29e7e0
Ashley Caselli <ashley@knowledgepixels.com>
2025-08-04 17:05:53 +0200

    test(NanopubImpl): add resources for testing the equals method in the two cases (text block in file with CRLF and LF)

--- src/test/java/org/nanopub/NanopubImplTest.java
+++ src/test/java/org/nanopub/NanopubImplTest.java
@@ -8,15 +8,24 @@ import java.io.IOException;
 import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class NanopubImplTest {
 
     @Test
-    void equals() throws IOException, MalformedNanopubException {
+    void equalsTextBlockWithSameLineSeparator() throws IOException, MalformedNanopubException {
         NanopubImpl nanopub1 = new NanopubImpl(new URL("https://w3id.org/np/RA6T-YLqLnYd5XfnqR9PaGUjCzudvHdYjcG4GvOc7fdpA"));
-        NanopubImpl nanopub2 = new NanopubImpl(new File("src/test/resources/np-grlc-query-test.trig"), RDFFormat.TRIG);
+        NanopubImpl nanopub2 = new NanopubImpl(new File("src/test/resources/RA6T-YLqLnYd5XfnqR9PaGUjCzudvHdYjcG4GvOc7fdpA.trig"), RDFFormat.TRIG);
 
         assertEquals(nanopub1, nanopub2);
+    }
+
+    @Test
+    void equalsTextBlockWithDifferentLineSeparator() throws IOException, MalformedNanopubException {
+        NanopubImpl nanopub1 = new NanopubImpl(new URL("https://w3id.org/np/RA6T-YLqLnYd5XfnqR9PaGUjCzudvHdYjcG4GvOc7fdpA"));
+        NanopubImpl nanopub2 = new NanopubImpl(new File("src/test/resources/RA6T-YLqLnYd5XfnqR9PaGUjCzudvHdYjcG4GvOc7fdpA-all-LF.trig"), RDFFormat.TRIG);
+
+        assertNotEquals(nanopub1, nanopub2);
     }
 
 }
