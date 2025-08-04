00daf167263882eb5e872ded5efab34f45e11b6b
Ashley Caselli <ashley@knowledgepixels.com>
2025-08-04 17:16:07 +0200

    test(NanopubImpl): add assertions to ensure that the nanopub is valid or not

--- src/test/java/org/nanopub/NanopubImplTest.java
+++ src/test/java/org/nanopub/NanopubImplTest.java
@@ -2,13 +2,13 @@ package org.nanopub;
 
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.junit.jupiter.api.Test;
+import org.nanopub.trusty.TrustyNanopubUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class NanopubImplTest {
 
@@ -17,6 +17,8 @@ class NanopubImplTest {
         NanopubImpl nanopub1 = new NanopubImpl(new URL("https://w3id.org/np/RA6T-YLqLnYd5XfnqR9PaGUjCzudvHdYjcG4GvOc7fdpA"));
         NanopubImpl nanopub2 = new NanopubImpl(new File("src/test/resources/RA6T-YLqLnYd5XfnqR9PaGUjCzudvHdYjcG4GvOc7fdpA.trig"), RDFFormat.TRIG);
 
+        assertTrue(TrustyNanopubUtils.isValidTrustyNanopub(nanopub1));
+        assertTrue(TrustyNanopubUtils.isValidTrustyNanopub(nanopub2));
         assertEquals(nanopub1, nanopub2);
     }
 
@@ -25,6 +27,8 @@ class NanopubImplTest {
         NanopubImpl nanopub1 = new NanopubImpl(new URL("https://w3id.org/np/RA6T-YLqLnYd5XfnqR9PaGUjCzudvHdYjcG4GvOc7fdpA"));
         NanopubImpl nanopub2 = new NanopubImpl(new File("src/test/resources/RA6T-YLqLnYd5XfnqR9PaGUjCzudvHdYjcG4GvOc7fdpA-all-LF.trig"), RDFFormat.TRIG);
 
+        assertTrue(TrustyNanopubUtils.isValidTrustyNanopub(nanopub1));
+        assertFalse(TrustyNanopubUtils.isValidTrustyNanopub(nanopub2));
         assertNotEquals(nanopub1, nanopub2);
     }
 
