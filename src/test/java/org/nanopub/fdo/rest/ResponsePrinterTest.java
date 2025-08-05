4712fdbaa82b39efb142d1f16494b875f16e42cd
Ashley Caselli <ashley@knowledgepixels.com>
2025-08-05 11:24:18 +0200

    test: add unit tests for ResponsePrinter

---
+++ src/test/java/org/nanopub/fdo/rest/ResponsePrinterTest.java
@@ -0,0 +1,45 @@
+package org.nanopub.fdo.rest;
+
+import org.junit.jupiter.api.Test;
+import org.nanopub.fdo.rest.gson.Data;
+import org.nanopub.fdo.rest.gson.ParsedJsonResponse;
+import org.nanopub.fdo.rest.gson.Value;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ResponsePrinterTest {
+
+    @Test
+    void printWithValidResponsePrintsExpectedOutput() {
+        ParsedJsonResponse response = new ParsedJsonResponse();
+        response.handle = "12345";
+        Data data1 = new Data();
+        data1.format = "JSON";
+        data1.value = "{\"key\":\"value\"}";
+        Value value1 = new Value();
+        value1.type = String.class.getSimpleName();
+        value1.data = data1;
+        response.values = new Value[]{
+                value1,
+        };
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
+
+        try {
+            ResponsePrinter.print(response);
+        } finally {
+            System.setOut(originalOut);
+        }
+
+        String expected = "id: 12345\n" +
+                "TYPE   DATA.FORMAT   DATA\n" +
+                "String   JSON   {\"key\":\"value\"}\n";
+        assertEquals(expected, outContent.toString());
+    }
+
+}
