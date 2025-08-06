73a13dc7e658410f2c7ca7e24474aa490b8d6cb8
Ashley Caselli <ashley@knowledgepixels.com>
2025-08-06 12:30:49 +0200

    test: add unit tests for SignatureUtils file path methods

---
+++ src/test/java/org/nanopub/extra/security/SignatureUtilsTest.java
@@ -0,0 +1,28 @@
+package org.nanopub.extra.security;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SignatureUtilsTest {
+
+    private final String pathSuffix = ".nanopub/";
+    private final String fileNamePrefix = "id";
+
+    @Test
+    void getFullFilePathFromHomePath() {
+        String pathPrefix = "~/";
+        String relativeFilePathAndFileNamePrefix = pathPrefix + pathSuffix + fileNamePrefix;
+        String fullFilePath = SignatureUtils.getFullFilePath(relativeFilePathAndFileNamePrefix);
+        assertEquals((System.getProperty("user.home") + "/" + pathSuffix), fullFilePath);
+    }
+
+    @Test
+    void getFullFilePathWithFullPath() {
+        String pathPrefix = "/home/user/";
+        String fullFilePath = pathPrefix + pathSuffix + fileNamePrefix;
+        String result = SignatureUtils.getFullFilePath(fullFilePath);
+        assertEquals(fullFilePath, result);
+    }
+
+}
