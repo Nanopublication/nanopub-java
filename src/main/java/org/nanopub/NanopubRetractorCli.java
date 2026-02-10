1fc6c5087f29fff28f6e7fff99b16bdde6b1fdf7
Tobias Kuhn <kuhntobias@gmail.com>
2026-02-10 16:59:16 +0100

    feat(NanopubRetractorCli): Support key file selection for 'retract'

--- src/main/java/org/nanopub/NanopubRetractorCli.java
+++ src/main/java/org/nanopub/NanopubRetractorCli.java
@@ -2,10 +2,15 @@ package org.nanopub;
 
 import com.beust.jcommander.ParameterException;
 import net.trustyuri.TrustyUriException;
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.RDFWriter;
 import org.eclipse.rdf4j.rio.Rio;
 import org.nanopub.extra.security.MalformedCryptoElementException;
+import org.nanopub.extra.security.SignNanopub;
+import org.nanopub.extra.security.SignatureAlgorithm;
 import org.nanopub.extra.security.TransformContext;
 import org.nanopub.extra.server.GetNanopub;
 import org.nanopub.extra.server.PublishNanopub;
@@ -15,6 +20,7 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
+import java.security.KeyPair;
 
 /**
  * <p>Command line interface for retraction of nanopublications.</p>
@@ -24,7 +30,8 @@ import java.security.GeneralSecurityException;
  *              it's the local path to a nanopub file.</li>
  *      <li>-p: optional - publish the created retraction (without -p the retraction nanopub will only be written to stdout)</li>
  * </ol>
- * <p>For signing the key at "~/.nanopub/id_rsa" is used and the orcid from "~/.nanopub/profile.yaml</p>
+ * <p>For signing, use -k to specify a custom key file path and -s to specify the signer's orcid IRI.
+ * Otherwise the key at "~/.nanopub/id_rsa" and the orcid from "~/.nanopub/profile.yaml" are used.</p>
  *
  * @since 1.80
  */
@@ -32,6 +39,12 @@ public class NanopubRetractorCli extends CliRunner {
 
     @com.beust.jcommander.Parameter(names = "-i", description = "Nanopub to be retracted, url or local path.", required = true)
     private String nanopubUrl;
+
+    @com.beust.jcommander.Parameter(names = "-k", description = "Path and file name of key files")
+    private String keyFilename;
+
+    @com.beust.jcommander.Parameter(names = "-s", description = "The orcid IRI of the signer")
+    private String signer;
 
     @com.beust.jcommander.Parameter(names = "-p", description = "directly publish the retraction")
     private boolean publish = false;
@@ -65,7 +78,8 @@ public class NanopubRetractorCli extends CliRunner {
      */
     public void run() throws MalformedNanopubException, IOException, TrustyUriException, GeneralSecurityException, MalformedCryptoElementException, NanopubAlreadyFinalizedException {
         Nanopub orig = getNanopubToRetract();
-        Nanopub retraction = NanopubRetractor.createRetraction(orig, TransformContext.makeDefault());
+        TransformContext tc = createTransformContext();
+        Nanopub retraction = NanopubRetractor.createRetraction(orig, tc);
         if (publish) {
             System.out.println("Publishing Retraction Nanopub.");
             PublishNanopub.publish(retraction);
@@ -73,6 +87,34 @@ public class NanopubRetractorCli extends CliRunner {
         // Write to System.out
         RDFWriter w = Rio.createWriter(RDFFormat.TRIG, new OutputStreamWriter(System.out, StandardCharsets.UTF_8));
         NanopubUtils.propagateToHandler(retraction, w);
+    }
+
+    private TransformContext createTransformContext() {
+        if (keyFilename == null && signer == null) {
+            return TransformContext.makeDefault();
+        }
+        ValueFactory vf = SimpleValueFactory.getInstance();
+        NanopubProfile profile = new NanopubProfile(NanopubProfile.IMPLICIT_PROFILE_FILE_NAME);
+        IRI signerIri = null;
+        if (signer != null) {
+            signerIri = vf.createIRI(signer);
+        } else if (profile.getOrcidId() != null) {
+            signerIri = vf.createIRI(profile.getOrcidId());
+        }
+        if (keyFilename == null) {
+            keyFilename = profile.getPrivateKeyPath();
+        }
+        if (keyFilename == null) {
+            keyFilename = TransformContext.DEFAULT_KEY_PATH;
+        }
+        SignatureAlgorithm algorithm = keyFilename.endsWith("_dsa") ? SignatureAlgorithm.DSA : SignatureAlgorithm.RSA;
+        KeyPair key;
+        try {
+            key = SignNanopub.loadKey(keyFilename, algorithm);
+        } catch (Exception ex) {
+            throw new RuntimeException("Could not load key: " + ex.getMessage(), ex);
+        }
+        return new TransformContext(algorithm, key, signerIri, false, false, false);
     }
 
     private Nanopub getNanopubToRetract() throws MalformedNanopubException, IOException {
