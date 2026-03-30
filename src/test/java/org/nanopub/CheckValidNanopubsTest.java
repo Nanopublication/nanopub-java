2656f468bcd48937f9c6e38f23be06e4e0c1b1ff
Ashley Caselli <ashley@knowledgepixels.com>
2026-03-30 20:29:39 +0200

    test(CheckValidNanopubsTest): refactor to use parameterized tests

--- src/test/java/org/nanopub/CheckValidNanopubsTest.java
+++ src/test/java/org/nanopub/CheckValidNanopubsTest.java
@@ -1,48 +1,73 @@
 package org.nanopub;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.nanopub.CheckNanopub.Report;
 import org.nanopub.testsuite.NanopubTestSuite;
 import org.nanopub.testsuite.TestSuiteEntry;
 import org.nanopub.testsuite.TestSuiteSubfolder;
 
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CheckValidNanopubsTest {
 
-    @Test
-    public void runTest() throws Exception {
-        for (TestSuiteEntry testFile : NanopubTestSuite.getLatest().getValid(TestSuiteSubfolder.PLAIN)) {
-            testPlain(testFile);
-        }
-        for (TestSuiteEntry testFile : NanopubTestSuite.getLatest().getValid(TestSuiteSubfolder.TRUSTY)) {
-            testTrusty(testFile);
-        }
-        for (TestSuiteEntry testFile : NanopubTestSuite.getLatest().getValid(TestSuiteSubfolder.SIGNED)) {
-            testSigned(testFile);
-        }
+    @DisplayName("Plain valid nanopubs")
+    @ParameterizedTest(name = "plain valid: {1}")
+    @MethodSource("plainValid")
+    public void testPlain(TestSuiteEntry testSuiteEntry, String name) {
+        runAndAssert(testSuiteEntry, Report::areAllValid);
     }
 
-    public void testPlain(TestSuiteEntry testSuiteEntry) throws Exception {
-        CheckNanopub c = CliRunner.initJc(new CheckNanopub(), new String[]{testSuiteEntry.toFile().getPath()});
-        Report report = c.check();
-        System.out.println(report.getSummary() + " " + testSuiteEntry);
-        assertTrue(report.areAllValid());
+    @DisplayName("Trusty valid nanopubs")
+    @ParameterizedTest(name = "trusty valid: {1}")
+    @MethodSource("trustyValid")
+    public void testTrusty(TestSuiteEntry testSuiteEntry, String name) {
+        runAndAssert(testSuiteEntry, Report::areAllTrusty);
     }
 
-    public void testTrusty(TestSuiteEntry testSuiteEntry) throws Exception {
-        CheckNanopub c = CliRunner.initJc(new CheckNanopub(), new String[]{testSuiteEntry.toFile().getPath()});
-        Report report = c.check();
-        System.out.println(report.getSummary() + " " + testSuiteEntry);
-        assertTrue(report.areAllTrusty());
+    @DisplayName("Signed valid nanopubs")
+    @ParameterizedTest(name = "signed valid: {1}")
+    @MethodSource("signedValid")
+    public void testSigned(TestSuiteEntry testSuiteEntry, String name) {
+        runAndAssert(testSuiteEntry, Report::areAllSigned);
     }
 
-    public void testSigned(TestSuiteEntry testSuiteEntry) throws Exception {
-        CheckNanopub c = CliRunner.initJc(new CheckNanopub(), new String[]{testSuiteEntry.toFile().getPath()});
-        Report report = c.check();
-        System.out.println(report.getSummary() + " " + testSuiteEntry);
-        assert report.areAllSigned();
-        assertTrue(report.areAllSigned());
+    static Stream<Arguments> plainValid() {
+        return NanopubTestSuite.getLatest()
+                .getValid(TestSuiteSubfolder.PLAIN)
+                .stream()
+                .map(e -> Arguments.of(e, e.getName()));
+    }
+
+    static Stream<Arguments> trustyValid() {
+        return NanopubTestSuite.getLatest()
+                .getValid(TestSuiteSubfolder.TRUSTY)
+                .stream()
+                .map(e -> Arguments.of(e, e.getName()));
+    }
+
+    static Stream<Arguments> signedValid() {
+        return NanopubTestSuite.getLatest()
+                .getValid(TestSuiteSubfolder.SIGNED)
+                .stream()
+                .map(e -> Arguments.of(e, e.getName()));
+    }
+
+    private void runAndAssert(TestSuiteEntry testSuiteEntry, Predicate<Report> condition) {
+        Report report;
+        try {
+            CheckNanopub c = CliRunner.initJc(new CheckNanopub(), new String[]{testSuiteEntry.toFile().getPath()});
+            report = c.check();
+            System.out.println(report.getSummary());
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+        assertTrue(condition.test(report));
     }
 
 }
