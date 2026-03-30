b6abfe790a24f0fa6befeb1a33f5a4f3827454f6
Ashley Caselli <ashley@knowledgepixels.com>
2026-03-30 20:26:46 +0200

    test(CheckInvalidNanopubsTest): convert to use parameterized tests

--- src/test/java/org/nanopub/CheckInvalidNanopubsTest.java
+++ src/test/java/org/nanopub/CheckInvalidNanopubsTest.java
@@ -1,30 +1,64 @@
 package org.nanopub;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.nanopub.CheckNanopub.Report;
 import org.nanopub.testsuite.NanopubTestSuite;
 import org.nanopub.testsuite.TestSuiteEntry;
 import org.nanopub.testsuite.TestSuiteSubfolder;
 
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class CheckInvalidNanopubsTest {
 
-    @Test
-    public void runTest() throws Exception {
-        for (TestSuiteEntry testFile : NanopubTestSuite.getLatest().getInvalid(TestSuiteSubfolder.PLAIN)) {
-            testPlain(testFile);
-        }
-        for (TestSuiteEntry testFile : NanopubTestSuite.getLatest().getInvalid(TestSuiteSubfolder.TRUSTY)) {
-            testTrusty(testFile);
-        }
-        for (TestSuiteEntry testFile : NanopubTestSuite.getLatest().getInvalid(TestSuiteSubfolder.SIGNED)) {
-            testSigned(testFile);
-        }
+    @DisplayName("Plain invalid nanopubs")
+    @ParameterizedTest(name = "plain invalid: {1}")
+    @MethodSource("plainInvalid")
+    public void testPlain(TestSuiteEntry testSuiteEntry, String name) {
+        runAndAssert(testSuiteEntry, Report::areAllValid);
     }
 
+    @DisplayName("Trusty invalid nanopubs")
+    @ParameterizedTest(name = "trusty invalid: {1}")
+    @MethodSource("trustyInvalid")
+    public void testTrusty(TestSuiteEntry testSuiteEntry, String name) {
+        runAndAssert(testSuiteEntry, Report::areAllTrusty);
+    }
 
-    public void testPlain(TestSuiteEntry testSuiteEntry) throws RuntimeException {
+    @DisplayName("Signed invalid nanopubs")
+    @ParameterizedTest(name = "signed invalid: {1}")
+    @MethodSource("signedInvalid")
+    public void testSigned(TestSuiteEntry testSuiteEntry, String name) {
+        runAndAssert(testSuiteEntry, Report::areAllSigned);
+    }
+
+    static Stream<Arguments> plainInvalid() {
+        return NanopubTestSuite.getLatest()
+                .getInvalid(TestSuiteSubfolder.PLAIN)
+                .stream()
+                .map(e -> Arguments.of(e, e.getName()));
+    }
+
+    static Stream<Arguments> trustyInvalid() {
+        return NanopubTestSuite.getLatest()
+                .getInvalid(TestSuiteSubfolder.TRUSTY)
+                .stream()
+                .map(e -> Arguments.of(e, e.getName()));
+    }
+
+    static Stream<Arguments> signedInvalid() {
+        return NanopubTestSuite.getLatest()
+                .getInvalid(TestSuiteSubfolder.SIGNED)
+                .stream()
+                .map(e -> Arguments.of(e, e.getName()));
+    }
+
+    private void runAndAssert(TestSuiteEntry testSuiteEntry, Predicate<Report> condition) {
         Report report;
         try {
             CheckNanopub c = CliRunner.initJc(new CheckNanopub(), new String[]{testSuiteEntry.toFile().getPath()});
@@ -33,31 +67,7 @@ public class CheckInvalidNanopubsTest {
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
-        assertFalse(report.areAllValid());
-    }
-
-    public void testTrusty(TestSuiteEntry testSuiteEntry) throws RuntimeException {
-        Report report;
-        try {
-            CheckNanopub c = CliRunner.initJc(new CheckNanopub(), new String[]{testSuiteEntry.toFile().getPath()});
-            report = c.check();
-            System.out.println(report.getSummary());
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
-        assertFalse(report.areAllTrusty());
-    }
-
-    public void testSigned(TestSuiteEntry testSuiteEntry) throws RuntimeException {
-        Report report;
-        try {
-            CheckNanopub c = CliRunner.initJc(new CheckNanopub(), new String[]{testSuiteEntry.toFile().getPath()});
-            report = c.check();
-            System.out.println(report.getSummary());
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
-        assertFalse(report.areAllSigned());
+        assertFalse(condition.test(report));
     }
 
 }
