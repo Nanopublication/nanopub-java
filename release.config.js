99772469c2e16f9c0df622a0ad4759464ed5e145
Ashley Caselli <ashley@knowledgepixels.com>
2026-04-22 15:08:29 +0200

    chore(release-config): add JAR file to GitHub release asset for CLI usage

--- release.config.js
+++ release.config.js
@@ -3,13 +3,14 @@ IMAGE_NAME_LOWER=$(echo "$IMAGE_NAME" | tr '[:upper:]' '[:lower:]')
 docker build --build-arg="VERSION=\${nextRelease.version}" -t "$IMAGE_NAME_LOWER:\${nextRelease.version}" -t "$IMAGE_NAME_LOWER:latest" .
 docker push --all-tags "$IMAGE_NAME_LOWER"
 `
-let config = require('semantic-release-preconfigured-conventional-commits');
-config.tagFormat = 'nanopub-${version}'
-config.branches = ['release']
+let config = require("semantic-release-preconfigured-conventional-commits");
+config.tagFormat = "nanopub-${version}"
+config.branches = ["release"]
 config.plugins.push(
   [
     "@terrestris/maven-semantic-release",
     {
+      "mavenTarget": "deploy",
       "settingsPath": "./settings.xml",
       "updateSnapshotVersion": true,
       "mvnw": true
@@ -21,7 +22,14 @@ config.plugins.push(
       "publishCmd": publishCmd
     }
   ],
-  "@semantic-release/github",
+  [
+    "@semantic-release/github",
+    {
+      "assets": [
+        {"path": "target/nanopub-*-jar-with-dependencies.jar", "label": "JAR for CLI usage"},
+      ]
+    }
+  ],
   "@semantic-release/git"
 )
 module.exports = config
